9f8c9da1bad2591c04896a7c6a46763262438ca2
Muhammad Shumyl Akbar <68348109+Shumi8@users.noreply.github.com>
2024-10-21 22:42:40 +0100

    Update Pipeline.py

--- Pipeline.py
+++ Pipeline.py
@@ -19,60 +19,60 @@ page_referrer_table = "your_database.your_schema.page_referrer"
 
 # COMMAND ----------
 
-GOOGLE_TOP_BRANDS_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/GoogleMerchant/TopBrands/'
+GOOGLE_TOP_BRANDS_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/xxxx/TopBrands/'
 GOOGLE_TOP_BRANDS_BRONZE_TABLE = 'your_database.google_merchant.top_brands_bronze'
 
-GOOGLE_TOP_PRODUCTS_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/GoogleMerchant/TopProducts/'
+GOOGLE_TOP_PRODUCTS_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/xxxx/TopProducts/'
 GOOGLE_TOP_PRODUCTS_BRONZE_TABLE = 'your_database.google_merchant.top_products_bronze'
 
-GOOGLE_TOP_PRODUCTS_INVENTORY_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/GoogleMerchant/TopProductsInventory/'
+GOOGLE_TOP_PRODUCTS_INVENTORY_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/xxxx/TopProductsInventory/'
 GOOGLE_TOP_PRODUCTS_INVENTORY_BRONZE_TABLE = 'your_database.google_merchant.top_products_inventory_bronze'
 
 GOOGLE_PRODUCTS_PRICE_BENCHMARKS_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/GoogleMerchant/PriceBenchmarks/'
 GOOGLE_PRODUCTS_PRICE_BENCHMARKS_BRONZE_TABLE = 'your_database.google_merchant.price_benchmarks_bronze'
 
-GOOGLE_PRODUCTS_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/GoogleMerchant/Products/'
+GOOGLE_PRODUCTS_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/xxxx/Products/'
 GOOGLE_PRODUCTS_BRONZE_TABLE = 'your_database.google_merchant.products_bronze'
 
-GOOGLE_BLACKLISTED_CATEGORIES_BRONZE_PATH = src_string+'/Bronze/Google/GoogleMerchant/Blacklisted/'
+GOOGLE_BLACKLISTED_CATEGORIES_BRONZE_PATH = src_string+'/xxxx/xxxx/GoogleMerchant/Blacklisted/'
 
-GOOGLE_PAGE_FEEDS_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/PageFeeds/'
+GOOGLE_PAGE_FEEDS_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/PageFeeds/'
 GOOGLE_PAGE_FEEDS_BRONZE_TABLE = 'your_database.google.page_feeds_bronze'
 
-GOOGLE_PAGE_METRICS_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/PageMetrics/'
+GOOGLE_PAGE_METRICS_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/PageMetrics/'
 GOOGLE_PAGE_METRICS_BRONZE_TABLE = 'your_database.google.page_metrics_bronze'
 
-GOOGLE_PAGE_REFERRER_BRONZE_TABLE_PATH = src_string+'/Bronze/Google/PageReferrer/'
+GOOGLE_PAGE_REFERRER_BRONZE_TABLE_PATH = src_string+'/xxxx/xxxx/PageReferrer/'
 GOOGLE_PAGE_REFERRER_BRONZE_TABLE = 'your_database.google.page_referrer_bronze'
 
 # COMMAND ----------
 
-GOOGLE_TOP_BRANDS_SILVER_TABLE_PATH = src_string+'/Silver/GoogleMerchant/TopBrands/'
+GOOGLE_TOP_BRANDS_SILVER_TABLE_PATH = src_string+'/xxxx/xxxx/TopBrands/'
 GOOGLE_TOP_BRANDS_SILVER_TABLE = 'your_database.google_merchant.top_brands_silver'
 
-GOOGLE_TOP_PRODUCTS_SILVER_TABLE_PATH = src_string+'/Silver/GoogleMerchant/TopProducts/'
+GOOGLE_TOP_PRODUCTS_SILVER_TABLE_PATH = src_string+'/xxxx/xxxx/TopProducts/'
 GOOGLE_TOP_PRODUCTS_SILVER_TABLE = 'your_database.google_merchant.top_products_silver'
 
-GOOGLE_PAGE_FEEDS_SILVER_TABLE_PATH = src_string+'/Silver/Google/PageFeeds/'
+GOOGLE_PAGE_FEEDS_SILVER_TABLE_PATH = src_string+'/xxxx/xxxx/PageFeeds/'
 GOOGLE_PAGE_FEEDS_SILVER_TABLE = 'your_database.google.page_feeds_silver'
 
-GOOGLE_PAGE_METRICS_SILVER_TABLE_PATH = src_string+'/Silver/Google/PageMetrics/'
+GOOGLE_PAGE_METRICS_SILVER_TABLE_PATH = src_string+'/xxxx/xxxx/PageMetrics/'
 GOOGLE_PAGE_METRICS_SILVER_TABLE = 'your_database.google.page_metrics_silver'
 
-GOOGLE_PAGE_REFERRER_SILVER_TABLE_PATH = src_string+'/Silver/Google/PageReferrer/'
+GOOGLE_PAGE_REFERRER_SILVER_TABLE_PATH = src_string+'/xxxx/xxxx/PageReferrer/'
 GOOGLE_PAGE_REFERRER_SILVER_TABLE = 'your_database.google.page_referrer_silver'
 
 # COMMAND ----------
 
-GOOGLE_TOP_BRANDS_GOLD_TABLE_PATH = src_string+'/Gold/GoogleMerchant/TopBrands/'
+GOOGLE_TOP_BRANDS_GOLD_TABLE_PATH = src_string+'/xxxx/xxxx/TopBrands/'
 GOOGLE_TOP_BRANDS_GOLD_TABLE = 'your_database.google_merchant.top_brands_gold'
 
-GOOGLE_TOP_PRODUCTS_GOLD_TABLE_PATH = src_string+'/Gold/GoogleMerchant/TopProducts/'
+GOOGLE_TOP_PRODUCTS_GOLD_TABLE_PATH = src_string+'/xxxx/xxxx/TopProducts/'
 GOOGLE_TOP_PRODUCTS_GOLD_TABLE = 'your_database.google_merchant.top_products_gold'
 
 # COMMAND ----------
 
-PRODUCT_TABLE_PATH = src_string+'/Output/CommercialExcellence/DataPipelines/Dimensions/GoldProductDim/'
+PRODUCT_TABLE_PATH = src_string+'/xxxx/xxxx/xxxx/xxxx/GoldProductDim/'
 
 # COMMAND ----------
 
